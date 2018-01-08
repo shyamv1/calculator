c954f0ad3abda322df620eb52b6e196497cc2749
sampath surineni <sampath.surineni@oracle.com>
2018-01-08 09:57:54 +0530

    included add function

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+from add import add
+
+
+fun_dict = {
+    '+': add
+}
+
 print('Hi I am a calculator')
 choice = 'Hi'
 
@@ -5,3 +12,5 @@ while choice != 'q':
     print('Type + for addition')
     print('Type - for subtractioin')
     choice = input().strip()
+    if choice in fun_dict:
+        print(fun_dict[choice]())
